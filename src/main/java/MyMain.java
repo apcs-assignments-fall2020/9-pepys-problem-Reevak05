6505f3160b7ea9f666aefcc21f5c5870a1b11175
Reevak05 <kaveergera@gmail.com>
2020-10-03 01:33:53 +0000

    Here have code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,26 +5,45 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int successes = 0;
+        for (int i = 0; i<100000; i++) {
+            int sixes = 0;
+            for (int j = 0; j<6; j++) if (((int)((Math.random()/0.1666666666666)) +1)==6) sixes +=1;
+            if (sixes>=1) successes +=1;
+        }
+        return 100*((double)successes/(double)100000);
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int successes = 0;
+        for (int i = 0; i<100000; i++) {
+            int sixes = 0;
+            for (int j = 0; j<12; j++) if (((int)((Math.random()/0.1666666666666)) +1)==6) sixes +=1;
+            if (sixes>=2) successes +=1;
+        }
+        return 100*((double)successes/(double)100000);
     }
 
     // Calculate the probability of rolling at least three 6's when rolling 
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int successes = 0;
+        for (int i = 0; i<100000; i++) {
+            int sixes = 0;
+            for (int j = 0; j<18; j++) if (((int)((Math.random()/0.1666666666666)) +1)==6) sixes +=1;
+            if (sixes>=3) successes +=1;
+        }
+        return 100*((double)successes/(double)100000);
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println("Probability of at least 1 six in 6 rolls: "+probabilityOneSix()+"%.");
+        System.out.println("Probability of at least 2 sixes in 12 rolls: "+probabilityTwoSixes()+"%.");
+        System.out.println("Probability of at least 3 sixes in 18 rolls: "+probabilityThreeSixes()+"%.");
+
+
     }
 }
